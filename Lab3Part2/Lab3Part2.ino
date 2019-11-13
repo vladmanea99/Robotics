3a386578bfe3604096957674be6bd61ac297c489
unknown <vladaron123@gmail.com>
2019-11-13 22:21:30 +0200

    Homework Lab 4 Buzzer

--- Lab3Part2/Lab3Part2.ino
+++ Lab3Part2/Lab3Part2.ino
@@ -1,6 +1,7 @@
 int potPin = A0;
 int ledPin = 5;
-
+bool ledState = false;
+int buzzerTone = 1000;
 void setup() {
   // put your setup code here, to run once:
   pinMode(potPin, INPUT);
@@ -10,9 +11,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int valPot = analogRead(potPin);
-  valPot = map(valPot, 0, 1023, 0, 255);
-  analogWrite(ledPin, valPot);
+  /*int valPot = analogRead(potPin);
+  //valPot = map(valPot, 0, 1023, 0, 255);
+  if (valPot < 300){
+    ledState = HIGH;
+  }
+  if (valPot > 500){
+    ledState = LOW;
+  }
+  digitalWrite(ledPin, ledState);
   Serial.println(valPot);
-
+*/
+tone(ledPin, buzzerTone, 500);
+delay(100);
+noTone(ledPin);
+delay(100);
+buzzerTone += 50;
 }
